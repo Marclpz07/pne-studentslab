f4088494facfedaf1859bc5302102b30fc633c46
marclpz07 <m.lopezpu.2025@alumnos.urjc.es>
2026-02-17 16:46:34 +0100

    no carpetas

---
+++ S04/header.py
@@ -0,0 +1,14 @@
+
+
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "sequences/RNU6_269P.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+filecontents= file_contents.split("\n")
+# -- Print the contents on the console
+
+print(filecontents[0])
