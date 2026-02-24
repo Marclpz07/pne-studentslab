225a09a0db243d29d707fa9ddc04e048bd19d195
marclpz07 <m.lopezpu.2025@alumnos.urjc.es>
2026-02-24 16:40:42 +0100

    no carpetas

---
+++ S06/e2.py
@@ -0,0 +1,38 @@
+from operator import itemgetter
+
+
+class Seq:
+    """A class for representing sequences"""
+
+    def __init__(self, strbases):
+
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        self.strbases = strbases
+        bases = ["A", "C", "G", "T"]
+        length = len(strbases)
+        i = 0
+        while i in range(0, length):
+            if strbases[i] in bases:
+                i += 1
+
+            else:
+                print("ERROR")
+                break
+        print("New sequence created!")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+def print_seqs():
+    for i in range()
+
+
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
+
---
+++ new.py
@@ -0,0 +1,21 @@
+class  Seq:
+    """A class for representing sequences"""
+    pass
+
+#Main program
+#create an object of the class Seq
+s1 = Seq()
+
+#create another object of the class Seq
+s2 = Seq()
+
+#constructor (__init__) method to initialize attributes
+def __init__(self, attribute1, attribute2):
+    self.attribute1 = attribute1
+    self.attribute2 = attribute2
+    #method to define actions and behaviours
+
+def method_name(self, parameter):
+
+    pass
+
